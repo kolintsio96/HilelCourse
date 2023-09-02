192c2bc91d360a2aa24444f84cc59f981948fbe4
Volodymyr Kolintso <vkolintso@itera-research.com>
2023-09-02 13:21:53 +0300

    add first app

---
+++ homework_1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.OutputEncoding = System.Text.Encoding.Unicode;
+Console.WriteLine("Мене звуть Володимир,\nЯ навчаюсь у школі Hillel");
+Console.ReadKey();
