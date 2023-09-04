fd2bf6a9a95e030c7872b9cc6802a13a7b40455f
Volodymyr Kolintso <vkolintso@itera-research.com>
2023-09-04 10:09:19 +0300

    fixed change number app

--- homework_2/ChangedNumber/Program.cs
+++ homework_2/ChangedNumber/Program.cs
@@ -5,6 +5,7 @@
     {
         static void Main(string[] args)
         {
+            // 1)
             char temp;
             Console.Write("Enter number: ");
             string input = Console.ReadLine();
@@ -14,6 +15,16 @@
             char_array[1] = temp;
             int num = int.Parse(new string(char_array.ToArray()));
             Console.WriteLine($"Changed number: {num}");
+
+            // 2)
+            Console.Write("Enter number: ");
+            string input2 = Console.ReadLine();
+            int[] numbers = input2.Select(o => Convert.ToInt32(o) - 48).ToArray();
+            numbers[0] = numbers[0] + numbers[1];
+            numbers[1] = numbers[0] - numbers[1];
+            numbers[0] = numbers[0] - numbers[1];
+            string[] result = numbers.Select(i => i.ToString()).ToArray();
+            Console.WriteLine($"Changed number: {String.Join("", result)}");
         }
     }
 }
