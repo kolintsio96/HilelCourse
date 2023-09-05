add3959a736d81e0832d91b3fd13da5acc5f01b6
Volodymyr Kolintso <vkolintso@itera-research.com>
2023-09-05 17:20:24 +0300

    added app for getting day of week

---
+++ homework_3/GetDayOfWeek/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace DayOfWeek
+{
+    //3) Написати програму, яка виводить назву дня неділі за його номером (1 - понеділок, 7 - неділя). 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayOfWeek = ReadNumber();
+            GetDayOfWeek(dayOfWeek);
+        }
+
+        private static int ReadNumber()
+        {
+            Console.Write("Enter number day of week:");
+            string input = Console.ReadLine();
+            bool succesfullParsing = int.TryParse(input, out int result);
+            if (!succesfullParsing)
+            {
+                Console.WriteLine("You enter wrong number");
+                return ReadNumber();
+            }
+            else if (result < 1 || result > 7)
+            {
+                Console.WriteLine("Please enter number from 1 to 7");
+                return ReadNumber();
+            }
+            return result;
+
+        }
+        private static void GetDayOfWeek(int number)
+        {
+            switch (number) { 
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+            }
+        }
+    }
+}
