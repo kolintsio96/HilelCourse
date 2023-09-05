17674f3850efb4824d5118a7f684626abecc659c
Volodymyr Kolintso <vkolintso@itera-research.com>
2023-09-05 17:00:51 +0300

    added checking even/odd numbers app

---
+++ homework_3/EvenOddNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace EvenOddNumbers
+{
+    //1) Написати програму, яка перевіряє введене число на парність/непарність. 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = ReadNumber();
+            CheckingEvenOddNumbers(number);
+        }
+
+        private static int ReadNumber()
+        {
+            Console.Write("Enter number:");
+            string input = Console.ReadLine();
+            bool succesfullParsing = int.TryParse(input, out int result);
+            if (!succesfullParsing) {
+                Console.WriteLine("You enter wrong number");
+                ReadNumber();
+            } else if(result == 0)
+            {
+                Console.WriteLine("Please enter number not equal 0");
+                ReadNumber();
+            }
+            return result;
+
+        }
+        private static void CheckingEvenOddNumbers(int number)
+        {
+            if (number % 2 == 0) {
+                Console.WriteLine("You enter even number");    
+            } else
+            {
+                Console.WriteLine("You enter odd number");    
+            }
+        }
+    }
+}
