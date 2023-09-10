2f6430f417e0ab933d92962f233c6d0245223b8d
Volodymyr Kolintso <vkolintso@itera-research.com>
2023-09-10 22:25:32 +0300

    refactoring simple number app

---
+++ homework_4/SimpleNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SimpleNumber
+{
+    //4) Перевірка чи є введене натуральне число простим
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int randomNumber = GetRandom();
+            bool isSimpleNumber = CheckNumber(randomNumber);
+            Console.WriteLine(isSimpleNumber ? "Simple" : "Not simple");  
+        }
+
+        private static bool CheckNumber(int randomNumber)
+        {
+            for (int i = 2; i < Math.Sqrt(randomNumber) + 1; i++)
+            {
+                if (randomNumber % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static int GetRandom()
+        {
+            Random random = new Random();
+            return random.Next(2, 1000);
+
+        }
+    }
+}
