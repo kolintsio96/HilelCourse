52dca762b2db9b9f29689feca9f8356f70569695
Volodymyr Kolintso <vkolintso@itera-research.com>
2023-09-10 16:03:00 +0300

    added app for checking password

---
+++ homework_4/CheckPassword/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace CheckPassword
+{
+    //1) Напишіть програму, яка «запитуватиме» правильний пароль, доки він не буде введений. Правильний пароль нехай буде "root". Якщо пароль неправильний, програма повинна сказати "Неправильний пароль!"
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCorrectPassword = ReadAndCheckPassword();
+            while (!isCorrectPassword)
+            {
+                Console.WriteLine("Wrong password!");
+                isCorrectPassword = ReadAndCheckPassword();
+            }
+            Console.Write("Password is correct!");
+        }
+
+        private static bool ReadAndCheckPassword()
+        {
+            string password = ReadPassword();
+            return CheckPassword(password);
+        }
+
+        private static string ReadPassword()
+        {
+            Console.Write("Enter password:");
+            return Console.ReadLine();
+
+        }
+
+        private static bool CheckPassword(string password)
+        {
+            return password == "root";
+        }
+    }
+}
