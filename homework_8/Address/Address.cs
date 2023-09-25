755ecfba3303efd4107263093e4af67f288f2a33
Volodymyr Kolintso <vkolintso@itera-research.com>
2023-09-25 11:19:54 +0300

    Added class Address

---
+++ homework_8/Address/Address.cs
@@ -0,0 +1,61 @@
+﻿namespace Address
+{
+    internal class Address
+    {
+		private string index;
+
+		public string Index
+		{
+			get { return index; }
+			set { if (value.Length == 5) index = value; }
+		}
+
+        private string city = "";
+
+        public string City
+        {
+            get { return city; }
+            set { if (!String.IsNullOrEmpty(value)) city = value; }
+        }
+        
+        private string country = "";
+
+        public string Country
+        {
+            get { return country; }
+            set { if (!String.IsNullOrEmpty(value)) country = value; }
+        }
+        
+        private string street = "";
+
+        public string Street
+        {
+            get { return street; }
+            set { if (!String.IsNullOrEmpty(value)) street = value; }
+        }
+        
+        private int house;
+
+        public int House
+        {
+            get { return house; }
+            set { if (value > 0) house = value; }
+        }
+        
+        private int apartment;
+
+        public int Apartment
+        {
+            get { return apartment; }
+            set { if (value > 0) apartment = value; }
+        }
+
+        public string GetAddress()
+        {
+            string fullAddress = $"{House} {Street} {Apartment}, {City}, {Country}, {Index}";
+            return fullAddress;
+
+        }
+
+    }
+}
---
+++ homework_8/Address/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Address
+{
+    //Створити клас із ім'ям Address
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string index = ReadConsole("Enter index: ");
+            string country = ReadConsole("Enter country: ");
+            string city = ReadConsole("Enter city: ");
+            string street = ReadConsole("Enter street: ");
+            int house = ReadConsole("Enter house: ", true);
+            int apartment = ReadConsole("Enter apartment: ", true);
+            Address address = new Address() { Index= index, Country= country, City= city, Street= street, House= house, Apartment= apartment };
+            Console.WriteLine(address.GetAddress());
+        }
+
+        private static int ReadConsole(string message, bool isNumber)
+        {
+            Console.Write(message);
+            string input = Console.ReadLine();
+            bool succesfullParsing = int.TryParse(input, out int result);
+            if (!succesfullParsing)
+            {
+                Console.WriteLine("You enter wrong number");
+                return ReadConsole(message, isNumber);
+            }
+            return result;
+
+        }
+
+        private static string ReadConsole(string message)
+        {
+            Console.Write(message);
+            string input = Console.ReadLine();
+            if (String.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Field can not be null or empty");
+                return ReadConsole(message);
+            }
+            return input;
+
+        }
+    }
+}
