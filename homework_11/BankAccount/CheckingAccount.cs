906c8e92b5ed6530e978cff598f8f9b9290f54c4
Volodymyr Kolintso <vkolintso@itera-research.com>
2023-10-12 13:57:12 +0300

    Added BankAccount app

---
+++ homework_11/BankAccount/CheckingAccount.cs
@@ -0,0 +1,40 @@
+﻿namespace BankAccount
+{
+    internal class CheckingAccount : BankAccount
+    {
+        double overdraftLimit;
+        public CheckingAccount(string accountHolder, double balance, double overdraftLimit) : base(accountHolder, balance)
+        {
+            this.overdraftLimit = overdraftLimit;
+        }
+        public override double Deposit(double amount)
+        {
+            balance += amount;
+            return balance;
+        }
+
+        public override void DisplayAccountInfo()
+        {
+            Console.WriteLine($"Balance = {balance}");
+            Console.WriteLine($"AccountHolder = {accountHolder}");
+            Console.WriteLine($"AccountNumber = {accountNumber}");
+            Console.WriteLine($"OverdraftLimit = {overdraftLimit}");
+        }
+
+        public override double Withdraw(double amount)
+        {
+            try {
+                if (amount > (balance + overdraftLimit))
+                {
+                    throw new Exception("Amount more then balance + overdraftLimit");
+                }
+                balance -= amount;
+
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+            return balance;
+        }
+    }
+}
---
+++ homework_11/BankAccount/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string owner = ReadInput("Enter owner name: ");  
+            double balance = ReadInput("Enter your start balance: ", true);  
+            double overdraftLimit = ReadInput("Enter overdraft limit: ", true);  
+            BankAccount bankCheckingAccount = new CheckingAccount(owner, balance, overdraftLimit);
+            bankCheckingAccount.DisplayAccountInfo();
+            double deposit = ReadInput("Enter summ of deposit: ", true);
+            double withdraw = ReadInput("Enter summ of withdraw: ", true);
+            bankCheckingAccount.Deposit(deposit);
+            bankCheckingAccount.Withdraw(withdraw);
+            bankCheckingAccount.DisplayAccountInfo();
+        }
+
+        private static double ReadInput(string message, bool isNumber)
+        {
+            Console.Write(message);
+            string input = Console.ReadLine();
+            bool succesfullParsing = double.TryParse(input, out double result);
+            try
+            {
+                if (!succesfullParsing)
+                {
+                    throw new Exception("You enter wrong number");
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ReadInput(message, isNumber);
+            }
+
+        }
+        private static string ReadInput(string message)
+        {
+            Console.Write(message);
+            string input = Console.ReadLine();
+            try
+            {
+                if (String.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Field can not be empty");
+                    return ReadInput(message);
+                }
+                return input;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ReadInput(message);
+            }
+
+        }
+    }
+}
